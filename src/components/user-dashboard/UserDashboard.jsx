f81b992b9fce9ce1dcb3a7813af109f28ec888a9
Bennie <kamoninjoki@gmail.com>
2024-08-06 18:39:06 +0300

    Adds Responsive Layout

--- src/components/user-dashboard/UserDashboard.jsx
+++ src/components/user-dashboard/UserDashboard.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import Sidebar from '../sidebar/Sidebar';
 import { useAuth } from '../../contexts/AuthContext';
 import WelcomeMessage from '../home/WelcomeMessage';
-import {Grid, Header, Segment} from 'semantic-ui-react'
+import { Grid, Header, Segment } from 'semantic-ui-react';
 
 import './UserDashboard.css';
 import TrackingTool from '../tracking/TrackinTool';
 import TypingEffect from '../TypingEffect';
 import SummaryMenu from './SummaryMenu';
-
-
-
 
 const UserDashboard = () => {
     const { userRole } = useAuth();
@@ -19,79 +16,39 @@ const UserDashboard = () => {
         switch (userRole) {
             case 'client':
                 return (
-// ------------------------------------------------Client-Content-----------------------------------------------------------
-                    <div>
-                        <div className="Summarycontainer">
-                            
+                    <div className="content">
+                        <div className="summary-container">
                             <div className="grid-item">
                                 <TrackingTool />
-                                <TypingEffect style={{color: 'red'}}
-      texts={["Track Your Package with Ease", "Get Live Location Updates", "Choose Your Preferred Courier", "Receive a Free Quote Instantly", "Reliable Support at Your Fingertips"]}
-      speed={100} // Speed in milliseconds
-      pause={1000} // Pause for 1 second after typing
-    />
-                                
+                                <TypingEffect 
+                                    style={{ color: 'red' }}
+                                    texts={[
+                                        "Track Your Package with Ease", 
+                                        "Get Live Location Updates", 
+                                        "Choose Your Preferred Courier", 
+                                        "Receive a Free Quote Instantly", 
+                                        "Reliable Support at Your Fingertips"
+                                    ]}
+                                    speed={100} 
+                                    pause={1000} 
+                                />
                             </div>
                             <div className="grid-item">
-                               <SummaryMenu />
-                            
+                                <SummaryMenu />
                             </div>
                         </div>
-
-
-                       
-               
                     </div>
-// ------------------------------------------------Client-Content-END-----------------------------------------------------------                    
                 );
             case 'courier':
                 return (
-                    <div>
-                        <div className="summarycontainer">
-                        <Sidebar />
-                           
-                           {/* Add component here */}
-                            {/* Note: AdminFinanceSummary is not included here for 'courier' */}
-                        </div>
-                        <div className="contentcontainer">
-                            <div className="left-column">
-                               {/* Add component here */}
-                            </div>
-                            <div className="left-column">
-                                <div className="top-card">
-                                 {/* Add component here */}
-                                </div>
-                                <div className="bottom-card">
-                                   {/* Add component here */}
-                                </div>
-                            </div>
-                        </div>
+                    <div className="content">
+                        {/* Add courier-specific components here */}
                     </div>
                 );
-                
- // ------------------------------------------------Client-Content-END-----------------------------------------------------------   
             case 'admin':
                 return (
-                    <div>
-                        <div className="summarycontainer">
-                        <Sidebar />
-                         
-                          {/* Add component here */}
-                    
-                        </div>
-                        <div className="content-container">
-                            <div className="left-column">
-                             {/* Add component here */}
-                            </div>
-                            <div className="left-column">
-                                <div className="top-card">
-                                {/* Add component here */}
-                                </div>
-                                <div className="bottom-card">
-                                 {/* Add component here */}
-                                </div>
-                            </div>
-                        </div>
+                    <div className="content">
+                        {/* Add admin-specific components here */}
                     </div>
                 );
             default:
@@ -106,7 +63,6 @@ const UserDashboard = () => {
 
     return (
         <div className="user-dashboard">
-            
             <div className="content-card">
                 <Sidebar />
                 {renderContent()}
